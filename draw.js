26f11d6161ad116d0b51054cfd9328dcfaf80f2c
danielchao0214 <34559279+danielchao0214@users.noreply.github.com>
2020-02-15 10:35:24 -0800

    asdf
    start

--- draw.js
+++ draw.js
@@ -1,2 +1,20 @@
-var draw = SVG('drawing').size(300, 300)
-var rect = draw.rect(100, 100).attr({ fill: '#f06' })
+// initialize SVG.js
+var draw = SVG('drawing');
+
+// event handler function
+function handler(e) {
+    e = e || window.event;
+
+    var pageX = e.pageX;
+    var pageY = e.pageY;
+
+    console.log(pageX, pageY);
+
+    draw.path()
+}
+
+// attach handler to the click event of the document
+document.addEventListener('mousemove', handler);
+
+// draw pink square
+draw.rect(100, 100).move(100, 50).fill('#f06');
