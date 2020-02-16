bcbc5ea39a788c0145b739c0d85e7192e80fda24
danielchao0214 <34559279+danielchao0214@users.noreply.github.com>
2020-02-16 01:48:53 -0800

    add graph

--- script.js
+++ script.js
@@ -7,141 +7,149 @@ let divApplier = rangy.createClassApplier('div');
 
 spanHighlighter.addClassApplier(spanApplier);
 divHighlighter.addClassApplier(divHighlighter);
-
+let graphCount = 0;
 function isGraphable(s){
     let calculator = Desmos.GraphingCalculator();
-    calculator.setExpression({ id: 'graph1', latex: s});
+    calculator.setExpression({id:'graph1', latex: s});
     calculator.observe('expressionAnalysis', function() {
         let analysis = calculator.expressionAnalysis["graph1"];
-        return analysis.isGraphable;
+        if(analysis.isGraphable){
+            let graphDiv = $('<div id="graph'+ graphCount +'"></div>');
+            doc.$textArea.append(graphDiv);
+            graphCount++;
+
+            let elt = $('#graph' + graphCount);
+            calculator = Desmos.GraphingCalculator(elt);
+            calculator.setExpression({id:'graph2', latex: s});
+
+            graphDiv.children().css('background', '#FFEFD9');
+        }
     });
 }
 
 function getGraphableParts(s){
     // to be implemented 
 }
-
-getGraphable("x=7");
-
+console.log(isGraphable("x"));
 
 
-let doc = {
-    typing: true,
-    selection,
-    $textArea: $('#textArea'),
-    toggle: (className)=>{
 
-    },
-    makeItalic: ()=>{
-        spanHighlighter.highlight('italic');
-    },
+// let doc = {
+//     typing: true,
+//     selection,
+//     $textArea: $('#textArea'),
+//     toggle: (className)=>{
 
-    makeBold: ()=>{
-        spanHighlighter.highlight('bold');
-    },
+//     },
+//     makeItalic: ()=>{
+//         spanHighlighter.highlight('italic');
+//     },
 
-    highlight: ()=>{
-        spanHighlighter.highlight('highlight');
-    },
-    makeStrikethrough: ()=>{
-        spanHighlighter.highlight('strikethrough');
-    },
-    addCheckBox: ()=>{
-        rangey.createRange().insertNode('<input type="checkbox">');
-    },
-    addUrl: (url)=>{
-        spanHighlighter.highlight('bold');
-    }
-}
+//     makeBold: ()=>{
+//         spanHighlighter.highlight('bold');
+//     },
 
-/**doc.$textArea.on('mouseup', 'keyup', function(e) {
-    if(typingMode){
-        doc.selection  = rangey.
-    }
-});**/
-
-doc.$textArea.on('input', ()=>{
-    console.log('hi');
-    let currText = $textArea.val();
-    let prevText = currText.slice(0, cursorPos);
-    const cursorPos = $textArea.selectionStart;
-    
-
-    const patterns = [
-        [/(^|[\r\n])(-|*)( |\t)$/, ()=>{
-            doc.makeBullet(PLAIN);
-        }],
-        [/`(la)?tex$/, doc.makeTex],
-        [/'img$/, doc.insertImg],
-        [/(^|[\r\n])1(\)|.)$/, ()=>{
-            doc.makeBullet(NUM);
-        }],
-        [/(^|[\r\n])i(\)|.)$/i, ()=>{
-            doc.makeBullet(ROMAN);
-        }],
-        [/(^|[\r\n])a(\)|.)$/i, ()=>{
-            doc.makeBullet(LETTER);
-        }],
-        [/'(b|(bold)|(strong))$/i, doc.makeBold],
-        [/'(i|(em)|(italics?))$/i, doc.makeItalic],
-        [/'(u(nderline)?)$/i, doc.underline],
-        [/'c(ode)?$/i, doc.addCode],
-        [/'table$/i, doc.addTable],
-        [/'check(box)?$/, doc.addCheckBox],
-        [/'h(ighlight)?$/, doc.addUrl],
-        [/'url$/, doc.addUrl],
-        [/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/, (url)=>{
-            doc.addUrl(url);
-        }]
-    ];
-    for(pattern of patterns){
-        const matches = prevText.exec(pattern[0]);
-        console.log(matches);
-        if(matches){
-            const match = matches[0];
-            pattern[1](match);
-            currText = currText.slice;
-            $textArea.value = prevtext.slice(0, match.firstIndex) + currText.slice(cursorPos);
-            return;
-        }
-    }
-});
-
-// const keywords =    ["e", "pi", 
-//                     "+", "-", "*", "/", "^",
-//                     "sin", "cos", "tan", "cot", "sec", "csc",
-//                     "arcsin", "arccos", "arctan", "arccot", "arcsec", "arccsc",
-//                     "ln", "log", "sqrt",
-//                     "x", "y"
-//                     ];
-
-// function getLatexParts(s){
-//     s.includes("=")
+//     highlight: ()=>{
+//         spanHighlighter.highlight('highlight');
+//     },
+//     makeStrikethrough: ()=>{
+//         spanHighlighter.highlight('strikethrough');
+//     },
+//     addCheckBox: ()=>{
+//         rangey.createRange().insertNode('<input type="checkbox">');
+//     },
+//     addUrl: (url)=>{
+//         spanHighlighter.highlight('bold');
+//     }
 // }
 
+// /**doc.$textArea.on('mouseup', 'keyup', function(e) {
+//     if(typingMode){
+//         doc.selection  = rangey.
+//     }
+// });**/
+
+// doc.$textArea.on('input', ()=>{
+//     console.log('hi');
+//     let currText = $textArea.val();
+//     let prevText = currText.slice(0, cursorPos);
+//     const cursorPos = $textArea.selectionStart;
+    
+
+//     const patterns = [
+//         [/(^|[\r\n])(-|*)( |\t)$/, ()=>{
+//             doc.makeBullet(PLAIN);
+//         }],
+//         [/`(la)?tex$/, doc.makeTex],
+//         [/'img$/, doc.insertImg],
+//         [/(^|[\r\n])1(\)|.)$/, ()=>{
+//             doc.makeBullet(NUM);
+//         }],
+//         [/(^|[\r\n])i(\)|.)$/i, ()=>{
+//             doc.makeBullet(ROMAN);
+//         }],
+//         [/(^|[\r\n])a(\)|.)$/i, ()=>{
+//             doc.makeBullet(LETTER);
+//         }],
+//         [/'(b|(bold)|(strong))$/i, doc.makeBold],
+//         [/'(i|(em)|(italics?))$/i, doc.makeItalic],
+//         [/'(u(nderline)?)$/i, doc.underline],
+//         [/'c(ode)?$/i, doc.addCode],
+//         [/'table$/i, doc.addTable],
+//         [/'check(box)?$/, doc.addCheckBox],
+//         [/'h(ighlight)?$/, doc.addUrl],
+//         [/'url$/, doc.addUrl],
+//         [/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/, (url)=>{
+//             doc.addUrl(url);
+//         }]
+//     ];
+//     for(pattern of patterns){
+//         const matches = prevText.exec(pattern[0]);
+//         console.log(matches);
+//         if(matches){
+//             const match = matches[0];
+//             pattern[1](match);
+//             currText = currText.slice;
+//             $textArea.value = prevtext.slice(0, match.firstIndex) + currText.slice(cursorPos);
+//             return;
+//         }
+//     }
+// });
+
+// // const keywords =    ["e", "pi", 
+// //                     "+", "-", "*", "/", "^",
+// //                     "sin", "cos", "tan", "cot", "sec", "csc",
+// //                     "arcsin", "arccos", "arctan", "arccot", "arcsec", "arccsc",
+// //                     "ln", "log", "sqrt",
+// //                     "x", "y"
+// //                     ];
+
+// // function getLatexParts(s){
+// //     s.includes("=")
+// // }
 
 
-document.addEventListener('keydown', (event) => {
-  const keyName = event.key;
 
-  if (event.ctrlKey) {
-    // event.key is not 'Control' (different key is pressed),
-    // event.ctrlKey true if Ctrl key is pressed at the same time.
-    console.log(`Combination of ctrlKey + ${keyName}`);
-  } else if (event.altKey){
-    console.log(`Combination of altKey + ${keyName}`);
-  } else {
-    console.log(`Key pressed ${keyName}`);
-  }
-});
+// document.addEventListener('keydown', (event) => {
+//   const keyName = event.key;
 
-document.addEventListener('keyup', (event) => {
-  const keyName = event.key;
+//   if (event.ctrlKey) {
+//     // event.key is not 'Control' (different key is pressed),
+//     // event.ctrlKey true if Ctrl key is pressed at the same time.
+//     console.log(`Combination of ctrlKey + ${keyName}`);
+//   } else if (event.altKey){
+//     console.log(`Combination of altKey + ${keyName}`);
+//   } else {
+//     console.log(`Key pressed ${keyName}`);
+//   }
+// });
 
-  // As the user releases the Ctrl key,
-  // so event.ctrlKey is false.
-  if (keyName === 'Control') {
-    console.log('Control key was released');
-  }
-});
+// document.addEventListener('keyup', (event) => {
+//   const keyName = event.key;
 
+//   // As the user releases the Ctrl key,
+//   // so event.ctrlKey is false.
+//   if (keyName === 'Control') {
+//     console.log('Control key was released');
+//   }
+// });
