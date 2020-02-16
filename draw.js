49fb38cb9057fa6ef5a814e923b57d50b4fec2d1
danielchao0214 <34559279+danielchao0214@users.noreply.github.com>
2020-02-15 16:30:16 -0800

    add variables
    color and width constants

--- draw.js
+++ draw.js
@@ -2,6 +2,8 @@
 var draw = SVG('drawing');
 let mouseDown = false;
 let path;  
+const COLOR = '#f06';
+const WIDTH = 4;
 
 
 // RENAME THIS FUNCTION -dc
@@ -14,13 +16,16 @@ function handler(e) {
         console.log(e.pageX, e.pageY);
     }
 }
+
 document.onmousedown = function(e) { 
     mouseDown = true;
     path = draw.path('M'.concat(e.pageX.toString(),' ',e.pageY.toString()));
     path.fill('none');
-    path.stroke({ color: '#f06', width: 4, linecap: 'round', linejoin: 'round' });
+    path.stroke({ color: COLOR, width: WIDTH, linecap: 'round', linejoin: 'round' });
 }
+
 document.onmouseup = function() {
     mouseDown = false;
 }
+
 document.addEventListener('mousemove', handler);
