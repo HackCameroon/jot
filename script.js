78b0389e6b6ccefc9a8063ef991e796ae105bd44
Form-And-Function <error0x194@gmail.com>
2020-02-16 03:01:26 -0800

    improve regex

--- script.js
+++ script.js
@@ -76,9 +76,54 @@ let doc = {
     }
 });**/
 
-doc.$textArea.on('input', ()=>{
-    const cursorPos = doc.$textArea.selectionStart;
-    let currText = doc.$textArea.val();
+function getCaretCharacterOffsetWithin(element) {
+    var caretOffset = 0;
+    var doc = element.ownerDocument || element.document;
+    var win = doc.defaultView || doc.parentWindow;
+    var sel;
+    if (typeof win.getSelection != "undefined") {
+      sel = win.getSelection();
+      if (sel.rangeCount > 0) {
+        var range = win.getSelection().getRangeAt(0);
+        var preCaretRange = range.cloneRange();
+        preCaretRange.selectNodeContents(element);
+        preCaretRange.setEnd(range.endContainer, range.endOffset);
+        caretOffset = preCaretRange.toString().length;
+      }
+    } else if ((sel = doc.selection) && sel.type != "Control") {
+      var textRange = sel.createRange();
+      var preCaretTextRange = doc.body.createTextRange();
+      preCaretTextRange.moveToElementText(element);
+      preCaretTextRange.setEndPoint("EndToEnd", textRange);
+      caretOffset = preCaretTextRange.text.length;
+    }
+    return caretOffset;
+  }
+  
+  function getCaretPosition() {
+    if (window.getSelection && window.getSelection().getRangeAt) {
+      var range = window.getSelection().getRangeAt(0);
+      var selectedObj = window.getSelection();
+      var rangeCount = 0;
+      var childNodes = selectedObj.anchorNode.parentNode.childNodes;
+      for (var i = 0; i < childNodes.length; i++) {
+        if (childNodes[i] == selectedObj.anchorNode) {
+          break;
+        }
+        if (childNodes[i].outerHTML)
+          rangeCount += childNodes[i].outerHTML.length;
+        else if (childNodes[i].nodeType == 3) {
+          rangeCount += childNodes[i].textContent.length;
+        }
+      }
+      return range.startOffset + rangeCount;
+    }
+    return -1;
+  }
+
+$('#textArea, #textArea [contenteditable="true"]').on('input', (e)=>{
+    const cursorPos = getCaretPosition();
+    let currText = e.target.innerText;
     let prevText = currText.slice(0, cursorPos);
     
     console.log(cursorPos);
@@ -118,11 +163,12 @@ doc.$textArea.on('input', ()=>{
         console.log(prevText);
         console.log(matches);
         if(matches){
+
             console.log('matched');
+            e.target.innerText = prevtext.slice(0, match.firstIndex) + currText.slice(cursorPos);
             const match = matches[0];
             pattern[1](match);
             currText = currText.slice;
-            doc.$textArea.value = prevtext.slice(0, match.firstIndex) + currText.slice(cursorPos);
             return;
         }
     }
